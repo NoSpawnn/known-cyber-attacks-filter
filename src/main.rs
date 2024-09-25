b3e5d97851271f2b5e31bffdd6fd27694270cfce
NoSpawnn <78027161+NoSpawnn@users.noreply.github.com>
2024-09-25 16:30:56 +0100

    extract titles

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,39 @@ use std::{
 };
 
 fn main() -> std::io::Result<()> {
-    const FOLDER: &str = "./known-cyber-attacks";
-    let ignored = HashSet::from(["README.md", "Template", ".git"]);
+    let attacks_folder = "./known-cyber-attacks";
+    let ignored = HashSet::from(["Template", ".git"]);
 
+    let readme_paths = fs::read_dir(attacks_folder)?.filter_map(|entry| {
+        entry.ok().and_then(|entry| {
+            let t = entry.file_type().unwrap();
+
+            if !t.is_dir() || ignored.contains(entry.file_name().to_str()?) {
+                None
+            } else {
+                Some(entry.path().join("README.md"))
+            }
+        })
+    });
+
+    let mut titles = <Vec<String>>::new();
     let out_file = OpenOptions::new()
         .create(true)
         .write(true)
         .truncate(true)
         .open("Compile.md")?;
     let mut writer = BufWriter::new(out_file);
-    let paths = fs::read_dir(FOLDER)?.filter_map(|entry| {
-        entry.ok().and_then(
-            |entry| match ignored.contains(entry.file_name().to_str()?) {
-                true => None,
-                false => Some(entry.path().join("README.md")),
-            },
-        )
-    });
+    readme_paths.for_each(|readme_path| {
+        let title = &readme_path
+            .parent()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .split_once(' ')
+            .unwrap_or(("", ""))
+            .1;
+        titles.push(title.to_string());
 
-    paths.for_each(|readme_path| {
         if let Ok(file) = File::open(&readme_path) {
             let mut reader = BufReader::new(file);
             let _ = io::copy(&mut reader, &mut writer);
